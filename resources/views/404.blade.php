6d8ca568747f8fd464c2f9d8ce8b6b03191a3367
Juma <nashonhhh@gmail.com>
2024-02-23 19:57:32 +0300

    touches

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -43,6 +43,6 @@
 
     <div class="error-container">
         <h1>404</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
+        <p>Hello, the page you are looking for does not exist.</p>
         <a href="/">Go to Home</a>
     </div>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,4 @@
-<?php require ('cell/up.php'); ?>
-<?php require ('cell/nav.php'); ?>
-<?php require ('cell/head.php'); ?>
+
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p>You can reach us through our <a style="color:aqua;" href="https://nashon-juma.github.io/sample-portfolio">site</a></p>
