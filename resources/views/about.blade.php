0be19916cbb7aa15f1de6ecda7e0bf4227a726a0
Juma <nashonhhh@gmail.com>
2024-02-26 10:42:27 +0300

    thr

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>You can reach us through our <a style="color:aqua;" href="https://nashon-juma.github.io/sample-portfolio">site</a></p>
-    </main>
-    </div>
-<?php require ('cell/down.php'); ?>
