76a11b889048e67cf8bdaf39e65119ba7c9a07e1
Juma <nashonhhh@gmail.com>
2024-02-28 11:14:22 +0300

    jes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,22 @@ Route::get('/', function () {
     return view('index');
 });
 
-
-Route::get('post/{no}', function ($no) {
-    $post=file_get_contents(__DIR__."/../resources/posts/{ $no }.html");
-    return view('post',['post'=>$post]);
+Route::get('/post', function () {
+    return view('index');
 });
+
+
+Route::get('post/{num}', function ($no) {
+    $path=__DIR__."/../resources/posts/{$no}.html";
+    ddd($path);
+    if (!file_exists($path)){
+        return view('404');
+    }
+    $post=file_get_contents($path);
+    return view('post',['post'=>$post]);
+})->whereAlpha('num');
+
+
 
 
 
