a9af3197bc2412da90a7bec7d2cd9354c372af8e
juma <nahshonjumaz@gmail.com>
2024-04-24 22:51:43 +0300

    Update contact.v.php

--- views/contact.v.php
+++ views/contact.v.php
@@ -6,7 +6,6 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p>We are located opposite the Greenhouse along ngong road</p>
         </div>
-
     </main>
     </div>
 <?php require ('cell/down.php'); ?>
