f2a916229de55023890fdf9dbe120f2064e00046
juma <nahshonjumaz@gmail.com>
2024-06-05 20:51:22 +0300

    Update index.v.php

--- views/index.v.php
+++ views/index.v.php
@@ -1,7 +1,6 @@
 <?php require ('cell/up.php'); ?>
 <?php require ('cell/nav.php'); ?>
 <?php require ('cell/head.php'); ?>
-
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p>My name is Nashon</p>
